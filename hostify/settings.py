bf15e511f51423abe7269bcf8960561eceff5504
Patrick Odeh <vyltyan@gmail.com>
2025-01-09 11:24:46 +0100

    Fix static file serving and configuration

--- hostify/settings.py
+++ hostify/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [BASE_DIR / "static"]
+#STATICFILES_DIRS = [BASE_DIR / "static"]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
