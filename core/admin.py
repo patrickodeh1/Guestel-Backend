6af5b3bba7d9cbf84d8f55bff872d11ea8275eec
Patrick Odeh <vyltyan@gmail.com>
2025-01-09 02:10:39 +0100

    configured admin interface

--- core/admin.py
+++ core/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from .models import User, Amenity, Hotel, Room, Booking, Review
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
+admin.site.site_header = "Hostify Administration"
+admin.site.site_title = "Hostify Admin Portal"
+admin.site.index_title = "Welcome to Hostify Administration Dashboard"
 
 class UserAdmin(BaseUserAdmin):
     list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'is_superuser')
